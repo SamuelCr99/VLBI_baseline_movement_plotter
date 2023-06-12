43783fa929ebe5636e32f6dfeb530266be45dbe1
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-06-12 10:06:08 -0400

    Added more code

--- plot_VLBI_baseline_difference.py
+++ plot_VLBI_baseline_difference.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-reader = pd.read_csv('2023a_bas_apriori.bas', delim_whitespace=True, skiprows=2)
+reader = pd.read_csv('2023a_bas_apriori.csv', delim_whitespace=True)
 
-print(reader.loc(0))
+x = reader.loc[0]
+print(x)
