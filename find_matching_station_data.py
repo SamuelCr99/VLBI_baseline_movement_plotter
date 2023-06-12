930f3c217b4e4943fe43974a20dcfd9c28e0dc01
FilipHerbertsson <filher@student.chalmers.se>
2023-06-12 20:53:47 +0200

    Added plotting of trimmed data

--- find_matching_station_data.py
+++ find_matching_station_data.py
@@ -1,23 +1,29 @@
 import pandas as pd
 
 def find_matching_station_data(station1, station2):
+    # Read in CSV file as Pandas dataframes
     lines_as_df = pd.read_csv('2023a_bas_apriori.csv', delim_whitespace=True)
+
+    # Read in CSV file as plain text
     lines = open('2023a_bas_apriori.csv', 'r')
     lines = lines.readlines()
     rows_of_interest = []
 
-
+    # Finds all rows where both stations are present
     for i in range(len(lines_as_df)): 
         locations = lines_as_df.loc[i].locations
         split_locations = locations.split('/')
         if station1 in split_locations and station2 in split_locations:
             rows_of_interest.append(lines[i+1])
 
+    # Adds header to new CSV file
     f = open('matching_rows.csv', 'w')
     f.write('BAS date epoch year locations length length_sigma transverse transverse_sigma horizontal horizontal_sigma \n')
     f.close()
 
     f = open('matching_rows.csv', 'a')
+
+    # Writes matching rows to new CSV file
     for row in rows_of_interest:
         f.write(row)
 
--- plot_baseline.py
+++ plot_baseline.py
@@ -1,17 +1,57 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import math
 
-distance = []
-year = []
+def plot_lines(data_file):
 
-data = pd.read_csv('matching_rows.csv', delim_whitespace=True)
+    # Read in the data
+    distance = []
+    year = []
 
-for i in range(len(data)):
-    x = data.loc[i]
-    distance.append(data.loc[i].length)
-    year.append(data.loc[i].year)
-    
+    data = pd.read_csv(data_file, delim_whitespace=True)
 
-plt.plot(year, distance)
-plt.show()
+    for i in range(len(data)):
+        distance.append(data.loc[i].length)
+        year.append(data.loc[i].year)
 
+    # Trim the data
+    while(1):
+
+        # Get the residuals from the trendline
+        trendline = np.polyfit(year, distance, 1)
+
+        residuals = []
+        standard_deviation = 0
+
+        for i in range(len(year)):
+            correct_distance = trendline[0]*year[i] + trendline[1]
+            residual = distance[i] - correct_distance
+            residuals.append(residual)
+            standard_deviation += pow(residual,2)
+        standard_deviation = math.sqrt(standard_deviation/len(year))
+        print(standard_deviation)
+
+        # Remove data with large deviation
+        distance_trimmed = []
+        year_trimmed = []
+
+        for i in range(len(year)):
+            if abs(residuals[i]) < standard_deviation*2:        # Two sigma is considered "large" deviation, check with John
+                distance_trimmed.append(distance[i])
+                year_trimmed.append(year[i])
+        if len(year) == len(year_trimmed):
+            break
+
+        year = year_trimmed
+        distance = distance_trimmed
+
+    # Plot baseline distance to time
+    plt.text(year_trimmed[int(len(year_trimmed)/4)], distance_trimmed[0], f'Total number of datapoints: {len(year_trimmed)}', fontsize = 12)
+    plt.plot(year_trimmed, distance_trimmed)
+    plt.xlabel('Year')
+    plt.ylabel('Distance [mm]') # Check with John that this is correct! 
+    plt.show()
+
+if __name__ == '__main__':
+    plot_lines('matching_rows.csv')
