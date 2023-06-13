4a0349a93c5d7f29bf39802650688677d6f0e686
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-06-13 14:22:44 -0400

    AutoPep 8 on files
    Co-authored-by: FilipHerbertsson

--- utility/find_all_mactching_stations.py
+++ utility/find_all_mactching_stations.py
@@ -2,13 +2,13 @@ import json
 from utility.find_matching_stations import find_matching_stations
 
 matching_stations_dict = {}
-with open('data/stations.txt') as file: 
+with open('data/stations.txt') as file:
     stations = file.readlines()
-    for station in stations: 
+    for station in stations:
         station = station.strip('\n')
         matching_stations = find_matching_stations(station)
         matching_stations_dict[station] = matching_stations
 
 
 with open('data/matching_stations.json', 'w') as file:
-    json.dump(matching_stations_dict, file, indent = 4)
+    json.dump(matching_stations_dict, file, indent=4)
