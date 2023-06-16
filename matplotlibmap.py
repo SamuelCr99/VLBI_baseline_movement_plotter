67c735437c4e47664c41b4e4dfb9ee513f863d24
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-06-16 15:19:36 -0400

    Fixed bug where names of locations would only show first time opening map
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- matplotlibmap.py
+++ matplotlibmap.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
-from matplotlib.backend_bases import MouseButton
 import numpy as np
 from PIL import Image
 import pandas as pd
+from matplotlib.backend_bases import MouseButton
 import math
 import warnings
 warnings.filterwarnings('ignore')
 
+# Global variables
 STATION_DRAW_RADIUS = 60.0
 STATION_CLICK_RADIUS = 1
 global_station_location = []
 selected_station = ""
 mouse_on_station = ""
-text = ""
+text = None
 
 def map_to_fig(coordinates):
     map_x = coordinates[0]
@@ -40,47 +41,37 @@ def fig_to_map(coordinates):
 
     return [map_x,map_y]
 
+def on_move(event):
+    if event.inaxes:
+        global mouse_on_station
+        global text
+        x,y = fig_to_map([event.xdata, event.ydata])
+        distance_to_stations = global_station_location.copy()
+        distance_to_stations["distance"] = distance_to_stations.apply(lambda s: math.sqrt(math.pow(s["x"]-x,2)+math.pow(s["y"]-y,2)), axis=1)
+        distance_to_stations.sort_values("distance", inplace=True)
+        distance_to_stations.reset_index(inplace=True)
+        old_mouse_on_station = mouse_on_station
+        if distance_to_stations.loc[0].distance <= STATION_CLICK_RADIUS:
+            mouse_on_station = distance_to_stations.loc[0]['station']
+        else:
+            mouse_on_station = ""
+
+        if (mouse_on_station != old_mouse_on_station):
+            text.set_text(mouse_on_station)
+            text.set_position([event.xdata+5, event.ydata + 5])
 
 def on_click(event):
-    global selected_station 
-    x,y = fig_to_map([event.xdata, event.ydata])
-    distance_to_stations = global_station_location.copy()
-    distance_to_stations["distance"] = distance_to_stations.apply(lambda s: math.sqrt(math.pow(s["x"]-x,2)+math.pow(s["y"]-y,2)), axis=1)
-    distance_to_stations.sort_values("distance", inplace=True)
-    distance_to_stations.reset_index(inplace=True)
-    if distance_to_stations.loc[0].distance <= STATION_CLICK_RADIUS:
-        selected_station = distance_to_stations.loc[0]['station']
-        plt.draw_all()
-        # fig.canvas.mpl_disconnect('button_press_event')
-        # fig.canvas.mpl_disconnect('motion_notify_event')
-        plt.close('all')
-        #plt.clf()
-        
-def on_move(event):
-    global mouse_on_station
-    global text
-    x,y = fig_to_map([event.xdata, event.ydata])
-    distance_to_stations = global_station_location.copy()
-    distance_to_stations["distance"] = distance_to_stations.apply(lambda s: math.sqrt(math.pow(s["x"]-x,2)+math.pow(s["y"]-y,2)), axis=1)
-    distance_to_stations.sort_values("distance", inplace=True)
-    distance_to_stations.reset_index(inplace=True)
-    old_mouse_on_station = mouse_on_station
-    if distance_to_stations.loc[0].distance <= STATION_CLICK_RADIUS:
-        mouse_on_station = distance_to_stations.loc[0]['station']
-    else:
-        mouse_on_station = ""
+    if event.button is MouseButton.LEFT:
+        global selected_station 
+        x,y = fig_to_map([event.xdata, event.ydata])
+        distance_to_stations = global_station_location.copy()
+        distance_to_stations["distance"] = distance_to_stations.apply(lambda s: math.sqrt(math.pow(s["x"]-x,2)+math.pow(s["y"]-y,2)), axis=1)
+        distance_to_stations.sort_values("distance", inplace=True)
+        distance_to_stations.reset_index(inplace=True)
+        if distance_to_stations.loc[0].distance <= STATION_CLICK_RADIUS:
+            selected_station = distance_to_stations.loc[0]['station']
+            plt.close('all')
 
-    if (mouse_on_station != old_mouse_on_station):
-        if text != "":
-            with plt.ion():
-                text.remove()
-                text = ""
-                print("I removed text!")
-        if mouse_on_station: 
-            with plt.ion():
-                plt.pause(0.01)
-                text = plt.text(event.xdata + 10, event.ydata + 10, mouse_on_station, size=12)
-                print(f"I added text: {mouse_on_station}")
 
 def draw_map(station_coordinates, title):
     global selected_station
@@ -93,30 +84,28 @@ def draw_map(station_coordinates, title):
     mouse_on_station = ""
 
     global text
-    text = ""
-
-    fig, ax = plt.subplots(figsize=(10,5), num=title)
-    
-    fig.canvas.mpl_connect('button_press_event', on_click)
-    fig.canvas.mpl_connect('motion_notify_event', on_move)
-
-    img = np.asarray(Image.open('world_map.png'))
-    plt.imshow(img)
 
     station_coordinates["fig_x"] = station_coordinates.apply(lambda s: map_to_fig([s.x,s.y])[0], axis=1)
     station_coordinates["fig_y"] = station_coordinates.apply(lambda s: map_to_fig([s.x,s.y])[1], axis=1)
+
+    # Draw the map
+    plt.ion()
+    _, ax = plt.subplots(figsize=(10,5), num=title)
+    text = plt.text(0, 0, "", size=12, color="black", backgroundcolor="white")
+    img = np.asarray(Image.open('world_map.png'))
+    plt.imshow(img)
     ax.scatter(station_coordinates.fig_x.to_list(),station_coordinates.fig_y.to_list(),STATION_DRAW_RADIUS,"black")
     ax.scatter(station_coordinates.fig_x.to_list(),station_coordinates.fig_y.to_list(),STATION_DRAW_RADIUS*0.5,"white")
     plt.axis('off')
     plt.tight_layout(pad=0)
-    plt.show()
-    plt.draw_all()
 
+    # Add the events to the event loop
+    plt.connect('motion_notify_event', on_move)
+    plt.connect('button_press_event', on_click)
+
+    # Show the map
+    plt.show(block=True)
+
+    # Return the selected station
     print(f'Selected station: {selected_station}')
-
-    fig.canvas.mpl_disconnect('button_press_event')
-    fig.canvas.mpl_disconnect('motion_notify_event')
-    plt.close('all')
-    print("I closed all plots!")
-    # plt.clf()
     return selected_station
