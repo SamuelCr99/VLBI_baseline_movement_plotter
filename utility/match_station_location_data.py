0c04aa7d02d9577656bb69119403eb17ee5321e7
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-06-20 09:52:41 -0400

    Updated program to use new map data

--- utility/match_station_location_data.py
+++ utility/match_station_location_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 with open('data/stations.txt', 'r') as file:
     our_stations = file.readlines()
 
-location_rows = pd.read_csv('data/station_locations_old.csv', delimiter=";")
+location_rows = pd.read_csv('data/position.csv', delim_whitespace=True)
 location_rows['updated'] = False
 
 
@@ -13,13 +13,20 @@ rows_to_write = []
 c = 0
 for i, loc_row in location_rows.iterrows():
     for our_station in our_stations:
-        if loc_row['station'].lower() == our_station.strip('\n').rstrip('_').lower():
-            location_rows.at[i, 'station'] = our_station.strip('\n')
+        if loc_row['Name'].lower() == our_station.strip('\n').rstrip('_').lower():
+            location_rows.at[i, 'Name'] = our_station.strip('\n')
             location_rows.at[i, 'updated'] = True
 location_rows = location_rows.loc[location_rows['updated']]
-location_rows.drop(['updated'], axis=1, inplace=True)
-location_rows.to_csv(
-    'data/station_locations_correct_names.csv', sep=",", index=False)
+location_rows.drop(['updated', 'ID', 'X', 'Y', 'Z', 'Occ.Code', 'Origin'], axis=1, inplace=True)
+location_rows.Lon = location_rows.Lon * -1
+location_rows = location_rows.rename(columns={'Name' : 'station', 'Lon': 'x', 'Lat': 'y'})
 
-# with open('data/station_locations_correct_names.csv', 'w') as file:
-#     file.writelines(rows_to_write)
+for i, loc_row in location_rows.iterrows():
+    if loc_row['x'] < -180:
+        location_rows.at[i, 'x'] = loc_row['x'] + 360
+    elif loc_row['x'] > 180:
+        location_rows.at[i, 'x'] = loc_row['x'] - 360
+
+
+location_rows.to_csv(
+    'data/station_locations.csv', sep=",", index=False)
