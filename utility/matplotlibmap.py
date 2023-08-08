dfdf38dd47c9984300f7e51bd84af715195c609e
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-08-08 11:31:11 -0400

    Added improved way of showing labels of map
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- utility/matplotlibmap.py
+++ utility/matplotlibmap.py
@@ -4,6 +4,7 @@ from PIL import Image
 from matplotlib.backend_bases import MouseButton
 import math
 import warnings
+import mplcursors
 warnings.filterwarnings('ignore')
 
 # Global variables
@@ -44,45 +45,6 @@ def fig_to_map(coordinates):
     return [map_x, map_y]
 
 
-def on_move(event):
-    '''
-    Shows station name when hovering over station.
-
-    Updates the text and position of the text element to follow the mouse.
-    Updates whenever the mouse hovers over something new.
-
-    Parameters:
-    event (event): All event information
-
-    Returns:
-    No return values!
-    '''
-    if event.inaxes:
-        global mouse_on_station
-        global text
-
-        # Gets location of click
-        x, y = fig_to_map([event.xdata, event.ydata])
-
-        # Sort the stations based on distance from mouse click
-        distance_to_stations = global_station_location.copy()
-        distance_to_stations["distance"] = distance_to_stations.apply(
-            lambda s: math.sqrt(math.pow(s["x"]-x, 2)+math.pow(s["y"]-y, 2)), axis=1)
-        distance_to_stations.sort_values("distance", inplace=True)
-        distance_to_stations.reset_index(inplace=True)
-
-        old_mouse_on_station = mouse_on_station
-
-        # Gives the station name of the station that the mouse is hovering over
-        if distance_to_stations.loc[0].distance <= STATION_CLICK_RADIUS:
-            mouse_on_station = distance_to_stations.loc[0]['station']
-        else:
-            mouse_on_station = ""
-
-        # Updates the station name text if a change occurred
-        if (mouse_on_station != old_mouse_on_station):
-            text.set_text(mouse_on_station)
-            text.set_position([event.xdata+5, event.ydata + 5])
 
 
 def on_click(event):
@@ -159,7 +121,7 @@ def draw_map(station_coordinates, title):
     plt.imshow(img)
 
     # Draw station locations on to screen
-    ax.scatter(station_coordinates.fig_x.to_list(),
+    station_points = ax.scatter(station_coordinates.fig_x.to_list(),
                station_coordinates.fig_y.to_list(), STATION_DRAW_RADIUS, "black")
     ax.scatter(station_coordinates.fig_x.to_list(
     ), station_coordinates.fig_y.to_list(), STATION_DRAW_RADIUS*0.5, "white")
@@ -168,8 +130,11 @@ def draw_map(station_coordinates, title):
     plt.tight_layout(pad=0)
 
     # Add the events to the event loop
-    plt.connect('motion_notify_event', on_move)
     plt.connect('button_press_event', on_click)
+    cursor_hover = mplcursors.cursor(
+            station_points, hover=mplcursors.HoverMode.Transient)
+    cursor_hover.connect(
+        "add", lambda sel: sel.annotation.set_text(station_coordinates.loc[sel.index].station))
 
     # Show the map
     plt.show(block=True)
